7dcf7477bb80cdc23e8533d6018b3743dd5412f6
Z-Jarrah <jarrahzeed@gmail.com>
2020-07-25 15:40:43 -0700

    Initial work

---
+++ dm_yelp_app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 25 2020
+
+@author: zeedjarrah
+"""
+
+# Return a dictionary of the dataset
+def load_dataset():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
